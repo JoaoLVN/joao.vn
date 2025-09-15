f453f927ebafa50af2a44cb0eea4b996fa1c01f7
João Luiz Vieira <joao.euu@gmail.com>
2025-09-15 13:03:28 +0200

    Remove snap start.

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ export function App() {
       <main className="flex flex-col w-full mx-auto px-4 sm:px-6">
         <section
           id="about"
-          className="relative min-h-dvh flex flex-col justify-center gap-5 snap-start max-w-lg mx-auto"
+          className="relative min-h-dvh flex flex-col justify-center gap-5 max-w-lg mx-auto"
         >
           <img
             src={joao}
@@ -62,7 +62,7 @@ export function App() {
         </section>
         <section
           id="resume"
-          className="snap-start min-h-dvh flex flex-col gap-10 max-w-xxl mx-auto py-5 justify-center"
+          className="min-h-dvh flex flex-col gap-10 max-w-xxl mx-auto py-5 justify-center"
         >
           <div className="flex items-center justify-between">
             <h2 className="text-3xl sm:text-4xl font-medium tracking-tight">
